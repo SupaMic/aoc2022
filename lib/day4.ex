acbcbcf95502f519fc5d1b7bf8e1d40389b7580f
Michael Maloney <maloneym@camosun.bc.ca>
2022-12-04 00:19:25 -0800

    solved day4 part1

--- lib/day4.ex
+++ lib/day4.ex
@@ -9,17 +9,43 @@ import Aoc2022
       %{
         puzzle: input("day4"),
         example: """
-  2-4,6-8
-  2-3,4-5
-  5-7,7-9
-  2-8,3-7
-  6-6,4-6
-  2-6,4-8
+2-4,6-8
+2-3,4-5
+5-7,7-9
+2-8,3-7
+6-6,4-6
+2-6,4-8
   """
       },
       input
     )
+  end
 
+  def part1(input) do
+    data(input)
+    |> input_lines_to_list()
+    |> Enum.reduce(0, fn sectors, acc ->
+          [sector_a, sector_b] = String.split(sectors, ",", trim: true)
+                    |> Enum.map(&sector_mapset/1)
+                    #|> IO.inspect(label: "sector_pair")
+
+          intersection = MapSet.intersection(sector_a, sector_b)
+                         # |> IO.inspect(label: "intersection")
+
+          cond do
+            MapSet.size(intersection) == MapSet.size(sector_a) -> acc + 1
+            MapSet.size(intersection) == MapSet.size(sector_b) -> acc + 1
+            true -> acc
+          end
+        end)
+
+  end
+
+  def sector_mapset(sector) do
+    [a, b] = String.split(sector, "-", trim: true)
+             |> Enum.map(&String.to_integer/1)
+
+    MapSet.new(a..b)
   end
 
 end
